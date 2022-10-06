6170bfcad6fff84d9575d0957f379c581425664f
thetrekmen <jonespm1.dev@gmail.com>
2022-10-06 13:33:04 -0400

    intercepts for b & m to find smallest error

--- linear_regression.py
+++ linear_regression.py
@@ -14,14 +14,34 @@ def calculate_error(m, b, point):
 # print(calculate_error(1, -1, (3, 3)))
 # print(calculate_error(-1, 1, (3, 3)))
 
-datapoints = [(1, 1), (3, 3), (5, 5), (-1, -1)]
+datapoints = [(1, 2), (2, 0), (3, 4), (4, 4), (5, 3)]
+
 def calculate_all_error(m, b, points):
-    error = 0
+    total_error = 0
     for point in points:
-        error = calculate_error(m, b, point)
-    return error
+        point_error = calculate_error(m, b, point)
+        total_error += point_error
+    return total_error
 # print(calculate_all_error(1, 0, datapoints))
 # print(calculate_all_error(1, 1, datapoints))
 # print(calculate_all_error(1, -1, datapoints))
 # print(calculate_all_error(-1, 1, datapoints))
 
+possible_ms = [m * 0.1 for m in range(-100, 101)]
+# print(possible_ms)
+possible_bs = [b * 0.1 for b in range(-200, 201)]
+# print(possible_bs)
+
+smallest_error = float("inf")
+best_m = 0
+best_b = 0
+
+for m in possible_ms:
+    for b in possible_bs:
+        error = calculate_all_error(m, b, datapoints)
+        if error < smallest_error:
+            best_m = m
+            best_b = b
+            smallest_error = error
+
+print(best_m, best_b, smallest_error)
